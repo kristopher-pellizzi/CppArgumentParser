5e2188f325870717a6a924b97148e84c2e42a21f
Kristopher Pellizzi <krijojo@iMacdiKistopher.homenet.telecomitalia.it>
2023-09-16 17:35:34 +0200

    Handle conversion error in case of append argument

--- src/IntConverter.cpp
+++ src/IntConverter.cpp
@@ -18,7 +18,8 @@ IntConverter* IntConverter::get_instance(){
 void IntConverter::convert(void* ptr, const string& s){
     try{
         *(int*) ptr = std::stoi(s);
-    } catch (std::exception& e){
+    } 
+    catch (std::exception& e){
         std::stringstream sstream;
         sstream << "Cannot convert value " << s << " to an integer" << std::endl;
         throw ArgumentConversionException(sstream.str(), s);
@@ -29,7 +30,14 @@ void IntConverter::convert(void* ptr, const std::vector<string>& v){
     std::vector<int> new_vect;
 
     for(unsigned i = 0; i < v.size(); ++i){
-        new_vect.push_back(std::stoi(v[i]));
+        try{
+            new_vect.push_back(std::stoi(v[i]));
+        } 
+        catch (std::exception& e){
+            std::stringstream sstream;
+            sstream << "Cannot convert value " << v[i] << " to an integer" << std::endl;
+            throw ArgumentConversionException(sstream.str(), v[i]);
+        }
     }
 
     *(std::vector<int>*) ptr = new_vect;
